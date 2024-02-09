ac84c509072dab5943b8ffdfb874f25d9f19df35
lmaurin- <leonard.maurin@gmail.com>
2024-02-09 19:09:58 +0100

    branch creation

--- backend/pong_game/urls.py
+++ backend/pong_game/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import gamemode_selection
+from .views import play, create_game, practice
 
 urlpatterns = [
-    path('', gamemode_selection, name='gamemode_selection'),
+    path('', play, name='play'),
+    path('create_game/', create_game, name='create_game'),
+    path('practice/', practice, name='practice'),
+
 ]
--- backend/pong_game/views.py
+++ backend/pong_game/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def gamemode_selection(request):
-	return render(request, 'gamemode_selection.html')
+def play(request):
+	return render(request, 'play.html')
+
+def create_game(request):
+	return render(request, 'create_game.html')
+
+def practice(request):
+	return render(request, 'practice.html')
