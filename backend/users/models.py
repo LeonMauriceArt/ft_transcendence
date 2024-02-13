be36f83010977a5f293f58f42eff47c81f347540
ValiSpac <spacvalentin1@gmail.com>
2024-02-13 22:07:27 +0100

    seting up WAF, load_module not working in nginx.conf

--- backend/users/models.py
+++ backend/users/models.py
@@ -5,11 +5,6 @@ DEFAULT = "-"
 class UserProfile(models.Model):
     login = models.CharField(max_length=255)
     password = models.CharField(max_length=128)
-<<<<<<< HEAD:users/models.py
     firstName = models.CharField(max_length=255, default="")
     lastName = models.CharField(max_length=255, default="")
-=======
-    firstName = models.CharField(max_length=255)
-    lastName = models.CharField(max_length=255)
->>>>>>> cef4db41275880a6c2accde5fc80df5bb3bcdb2c:backend/users/models.py
 
