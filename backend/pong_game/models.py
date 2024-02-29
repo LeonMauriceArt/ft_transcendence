6aaca733eb4f2e9bb24ed2707d7888ea75854424
Akhellad <ahmed.khelladi.pro@gmail.com>
2024-02-29 19:13:53 +0100

    indentation problem on match history model

--- backend/pong_game/models.py
+++ backend/pong_game/models.py
@@ -5,7 +5,7 @@ class MatchHistory(models.Model):
 	match_date = models.DateTimeField(default=timezone.now)
 	player_one = models.CharField(max_length=255, default="player_one")
 	player_two = models.CharField(max_length=255, default="player_two")
-    player_one_score = models.IntegerField(default=0)
-    player_two_score = models.IntegerField(default=0)
+	player_one_score = models.IntegerField(default=0)
+	player_two_score = models.IntegerField(default=0)
 	winner = models.CharField(max_length=255, default="winner")
 
