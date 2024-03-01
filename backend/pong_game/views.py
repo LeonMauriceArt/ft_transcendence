e4c6f208bd65eb07f2aa51bb0ba95296de96c526
Ahmed Khelladi <akhellad@z4r6p1.42lyon.fr>
2024-03-01 16:15:44 +0100

    login required for quickmatch

--- backend/pong_game/views.py
+++ backend/pong_game/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 def play(request):
 	return render(request, 'play.html')
 
+@login_required
 def quickmatch(request):
 	return render(request, 'quickmatch.html')
+	pass
 
 def join_game(request):
 	return render(request, 'join_game.html')
