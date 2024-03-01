37834182b33621c1a15cfb4a45b503d9e4036a7c
Ezio Giraldi <egiraldi@z2r6p8.42lyon.fr>
2024-03-01 17:37:59 +0100

    WIP TOURNAMENT - BACK FOR THE LOBBY DONE

--- backend/mystaticfiles/js/tournament/online_tournament.js
+++ backend/mystaticfiles/js/tournament/online_tournament.js
@@ -1,10 +1,62 @@
-const get_lobby_data = () => {
-    fetch(`/tournament/api/tournament/${localStorage.getItem('current_tournament_id')}/`, {
-        method: "GET"
-    }).then(response => {
-        if (!response.ok)
-            throw new Error('Can\'t get tournament info')
-        return response.json()
-    }).then(console.log)
+// VARIABLES
+
+const base_wssurl = 'ws://' + window.location.host + '/ws/tournament/'
+
+
+// SOCKET INCOMING EVENTS
+
+const on_message = (message) => {
+    handler = on_message_handlers.filter((e) => message.type === e.type)
+
+    handler[0]?.handler(message.arg)
+}
+
+const on_players_update = (arg) => {
+    console.log('PLAYERS UPDATE')
+    console.log(arg)
+}
+
+const on_message_handlers = [
+    { type: 'players_update', handler: on_players_update }
+]
+
+// PAGE LOADING
+
+const load_create_online = () => {
+    loadContent('/tournament/create_online_page', 'content')
+    .then(fetch_new_tournament_id)
+    .then(connect_socket)
+}
+
+const load_tournament_requests = () => {
+    loadContent('/tournament/tournament_requests_page', 'content')
+}
+
+// SERVER COMMUNICATIONS
+
+const fetch_new_tournament_id = () => {
+    return fetch('/tournament/api/create_tournament', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application.json'
+        }
+    }).then(response => response.json())
     .catch(console.error)
+}
+
+const connect_socket = ({ tournament_id }) => {
+    const socket = new WebSocket(base_wssurl + 'd')
+
+    socket.onopen = function(event) {
+        console.log("WebSocket connection opened:", event);
+    }
+
+    socket.onmessage = function(event) {
+        const message = JSON.parse(event.data);
+        on_message(message);
+    }
+
+    socket.onclose = function(event) {
+        console.log("WebSocket connection closed:", event);
+    }
 }
--- backend/tournament/views.py
+++ backend/tournament/views.py
@@ -1,69 +1,25 @@
 import json
+import uuid
 from django.shortcuts import render
 from django.http import JsonResponse
-from .models import Tournament, TournamentPlayer
-from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import get_object_or_404
 
-def play_tournament(request):
-    return render(request, 'play_tournament.html')
+# TEMPLATES
 
-def create_local(request):
-    return render(request, 'create_local.html')
+def tournament_page(request):
+    return render(request, 'tournament_page.html')
 
-def current_state(request):
-    return render(request, 'current_state.html')
+def create_local_page(request):
+    return render(request, 'create_local_page.html')
 
-def create_online(request):
-    return render(request, 'create_online.html')
+def create_online_page(request):
+    return render(request, 'create_online_page.html')
 
-def join_online(request):
-    return render(request, 'join_online.html')
+def tournament_requests_page(request):
+    return render(request, 'tournament_requests_page.html')
 
-def pre_lobby(request):
-    return render(request, 'pre_lobby.html')
+# API
 
-def lobby(request):
-    return render(request, 'lobby.html')
+def create_tournament(request):
+    tournament_id = str(uuid.uuid4())
 
-@csrf_exempt
-def api_tournament(request):
-    if request.method == 'GET':
-        tournaments = Tournament.objects.all()
-        tournament_list = [tournament.to_dict() for tournament in tournaments]
-        return JsonResponse(tournament_list, safe=False)
-    elif request.method == 'POST':
-        data = json.loads(request.body)
-        try:
-            tournament = Tournament.objects.create(
-                name=data['tournament_name']
-            )
-            return JsonResponse({'id': tournament.id}, status=201)
-        except KeyError:
-            return JsonResponse({'error': 'invalid format'}, status=400)
-    elif request.method == 'DELETE':
-        try:
-            Tournament.objects.all().delete()
-            return JsonResponse({'success': True, 'message': 'All tournaments deleted successfully.'})
-        except Exception as e:
-            return JsonResponse({'success': False, 'message': f'Error deleting tournaments: {str(e)}'}, status=500)
-
-@csrf_exempt
-def api_tournament_arg(request, tournament_id):
-    if request.method == 'PUT':
-        data = json.loads(request.body)
-        try:
-            tournament = Tournament.objects.get(id=tournament_id)
-            player = TournamentPlayer.objects.create(
-                name=data['aliase']
-            )
-            if player not in tournament.players.all() and tournament.players.count() < 4:
-                tournament.players.add(player)
-                return JsonResponse({ 'success': True, 'message': 'Player joined the lobby'})
-            else:
-                return JsonResponse({ 'success': False, 'message': 'Lobby full'}, status=400)
-        except (Tournament.DoesNotExist, KeyError):
-            return JsonResponse({'error': 'cant join tournament'}, status=400)
-    elif request.method == 'GET':
-        tournament = get_object_or_404(Tournament, id=tournament_id)
-        return JsonResponse(tournament.to_dict(), safe=False)
+    return JsonResponse({ 'tournament_id': tournament_id }) 
