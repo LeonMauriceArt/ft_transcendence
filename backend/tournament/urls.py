b223cd5d1afbda4e8d35607d2f852ed23c3008db
lmaurin- <leonard.maurin@gmail.com>
2024-03-12 14:28:02 +0100

    removed local tournament

--- backend/tournament/urls.py
+++ backend/tournament/urls.py
@@ -4,7 +4,6 @@ from . import consumers
 
 urlpatterns = [
     path('', views.tournament_page),
-    path('create_local_page/', views.create_local_page),
     path('create_online_page/', views.create_online_page),
     path('tournament_requests_page/', views.tournament_requests_page),
     path('invite_page/', views.invite_page),
